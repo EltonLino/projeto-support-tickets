fef40c75487e1124a6e3bae357fce3927f780990
Elton Lino <eltonblino@gmail.com>
2026-09-03 05:21:33 -0300

    criando middleware para ober body

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import http from "node:http" // Importo http nativo do node
 
-function listener (request, response) { // Inicio função do servidor
+import { jsonHandler } from "./middlewares/jsonHandler.js"
 
+async function listener (request, response) { // Inicio função do servidor
+    await jsonHandler(request, response)
 }
 
 http.createServer(listener).listen(3333) // Crio o servidor com createServer, passo a função para ele e configuro a porta que será executada
