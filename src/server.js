b57d3e514fd5983c3c19c6d0430d2e34f8ec9d4a
Elton Lino <eltonblino@gmail.com>
2026-09-04 05:22:41 -0300

    middleware de rotas

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import http from "node:http" // Importo http nativo do node
 
 import { jsonHandler } from "./middlewares/jsonHandler.js"
+import { routeHandler } from "./middlewares/routeHandler.js"
 
 async function listener (request, response) { // Inicio função do servidor
     await jsonHandler(request, response)
+    routeHandler(request, response)
 }
 
 http.createServer(listener).listen(3333) // Crio o servidor com createServer, passo a função para ele e configuro a porta que será executada
